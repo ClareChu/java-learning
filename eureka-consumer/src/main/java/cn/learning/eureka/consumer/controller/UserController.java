65db2f72aa1d9c66a5f972002dfd15845ab65531
ClareChu <1062186165@qq.com>
2019-05-09 13:50:46 +0800

    eureka

---
+++ eureka-consumer/src/main/java/cn/learning/eureka/consumer/controller/UserController.java
@@ -0,0 +1,11 @@
+package cn.learning.eureka.consumer.controller;
+
+/**
+ * @ClassName UserController
+ * @Description TODO
+ * @Author clare
+ * @Date 2019/5/9 13:41
+ * @Version 1.0
+ */
+public class UserController {
+}
---
+++ eureka-provider/src/main/java/cn/learning/provider/controller/UserController.java
@@ -0,0 +1,11 @@
+package cn.learning.provider.controller;
+
+/**
+ * @ClassName UserController
+ * @Description TODO
+ * @Author clare
+ * @Date 2019/5/9 13:30
+ * @Version 1.0
+ */
+public class UserController {
+}
