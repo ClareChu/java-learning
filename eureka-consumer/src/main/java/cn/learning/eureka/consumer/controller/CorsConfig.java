bd73e57bd5492dfa551a9595b91fc2076aa0d6d9
ClareChu <1062186165@qq.com>
2019-05-12 12:28:09 +0800

    health

---
+++ eureka-consumer/src/main/java/cn/learning/eureka/consumer/controller/CorsConfig.java
@@ -0,0 +1,11 @@
+package cn.learning.eureka.consumer.controller;
+
+/**
+ * @ClassName CorsConfig
+ * @Description TODO
+ * @Author clare
+ * @Date 2019/5/11 23:46
+ * @Version 1.0
+ */
+public class CorsConfig {
+}
---
+++ eureka-consumer/src/main/java/cn/learning/eureka/consumer/controller/User.java
@@ -0,0 +1,11 @@
+package cn.learning.eureka.consumer.controller;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author clare
+ * @Date 2019/5/11 23:08
+ * @Version 1.0
+ */
+public class User {
+}
